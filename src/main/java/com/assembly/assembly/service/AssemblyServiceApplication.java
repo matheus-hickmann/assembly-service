f1c1a486a169ec2a17ffb30d39703b547a3c4375
Matheus Hickmann <matheushickmann@MACBOOK-PRO-DE-MATHEUS.local>
2021-10-01 21:29:09 -0300

    Initial Commit

---
+++ src/main/java/com/assembly/assembly/service/AssemblyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.assembly.assembly.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssemblyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssemblyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assembly/assembly/service/AssemblyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assembly.assembly.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssemblyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
