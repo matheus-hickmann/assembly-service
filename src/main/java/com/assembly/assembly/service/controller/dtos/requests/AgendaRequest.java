615df147635ff925daeb728ffdf184a7f914c08f
Matheus Hickmann <hickmann.matheus@gmail.com>
2021-10-03 19:10:24 -0300

    feat: Create agendas requests and responses

---
+++ src/main/java/com/assembly/assembly/service/controller/dtos/requests/AgendaRequest.java
@@ -0,0 +1,24 @@
+package com.assembly.assembly.service.controller.dtos.requests;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(NON_NULL)
+public class AgendaRequest {
+
+    @NotBlank
+    private String name;
+    private String description;
+    @NotBlank
+    private String creatorDocument;
+
+}
---
+++ src/main/java/com/assembly/assembly/service/controller/dtos/responses/AgendaResponse.java
@@ -0,0 +1,19 @@
+package com.assembly.assembly.service.controller.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AgendaResponse {
+
+    UUID id;
+    String name;
+    String description;
+    String creatorDocument;
+
+}
