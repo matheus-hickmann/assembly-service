804ecd6db0031f0e8c38dd3a0ed18b46388b959a
Matheus Hickmann <hickmann.matheus@gmail.com>
2021-10-03 19:11:11 -0300

    feat: Create agenda services

---
+++ src/main/java/com/assembly/assembly/service/service/agenda/CreateAgendaService.java
@@ -0,0 +1,33 @@
+package com.assembly.assembly.service.service.agenda;
+
+import com.assembly.assembly.service.model.Agenda;
+import com.assembly.assembly.service.repository.AgendaRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class CreateAgendaService {
+
+    private final AgendaRepository repository;
+
+    @Autowired
+    public CreateAgendaService(
+            AgendaRepository repository
+    ) {
+        this.repository = repository;
+    }
+
+    public Agenda createAgenda(String name, String description, String creatorDocument) {
+
+        //TODO: Implement validations
+        return repository.save( Agenda.builder()
+                .name( name )
+                .creatorDocument( creatorDocument )
+                .description( description )
+                .build()
+        );
+    }
+
+}
---
+++ src/main/java/com/assembly/assembly/service/service/agenda/GetAllAgendasService.java
@@ -0,0 +1,23 @@
+package com.assembly.assembly.service.service.agenda;
+
+import com.assembly.assembly.service.model.Agenda;
+import com.assembly.assembly.service.repository.AgendaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GetAllAgendasService {
+
+    private final AgendaRepository repository;
+
+    @Autowired
+    public GetAllAgendasService(AgendaRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Agenda> getAllAgendas() {
+        return repository.findAll();
+    }
+}
