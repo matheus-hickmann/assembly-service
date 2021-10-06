ca773ea47e2eabe9a5dfa5ce8f5ba398de934338
Matheus Hickmann <hickmann.matheus@gmail.com>
2021-10-06 16:08:07 -0300

    feat: Add session response

---
+++ src/main/java/com/assembly/assembly/service/controller/dtos/responses/SessionResponse.java
@@ -0,0 +1,27 @@
+package com.assembly.assembly.service.controller.dtos.responses;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SessionResponse {
+
+    private UUID id;
+    private UUID agendaId;
+    private String agendaName;
+    private String agendaDescription;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy hh:mm:ss")
+    LocalDateTime startDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy hh:mm:ss")
+    LocalDateTime endDate;
+
+}
