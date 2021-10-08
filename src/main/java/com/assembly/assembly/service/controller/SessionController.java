9783f537dc86aa4729a126e65fd89b243b49c0c1
Matheus Hickmann <hickmann.matheus@gmail.com>
2021-10-07 23:35:12 -0300

    feat: Add endpoint

--- src/main/java/com/assembly/assembly/service/controller/SessionController.java
+++ src/main/java/com/assembly/assembly/service/controller/SessionController.java
@@ -2,9 +2,11 @@ package com.assembly.assembly.service.controller;
 
 import com.assembly.assembly.service.controller.dtos.requests.VoteRequest;
 import com.assembly.assembly.service.controller.dtos.responses.SessionResponse;
+import com.assembly.assembly.service.controller.dtos.responses.SessionResultResponse;
 import com.assembly.assembly.service.controller.dtos.responses.VoteResponse;
 import com.assembly.assembly.service.model.Session;
 import com.assembly.assembly.service.service.session.OpenSessionService;
+import com.assembly.assembly.service.service.vote.ComputeVoteService;
 import com.assembly.assembly.service.service.vote.VoteService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.slf4j.Slf4j;
@@ -22,12 +24,14 @@ public class SessionController {
 
     private final OpenSessionService openSessionService;
     private final VoteService voteService;
+    private final ComputeVoteService computeVoteService;
     private final ModelMapper modelMapper;
 
     @Autowired
-    public SessionController(OpenSessionService openSessionService,VoteService voteService,ModelMapper modelMapper) {
+    public SessionController(OpenSessionService openSessionService,VoteService voteService,ComputeVoteService computeVoteService,ModelMapper modelMapper) {
         this.openSessionService = openSessionService;
         this.voteService = voteService;
+        this.computeVoteService = computeVoteService;
         this.modelMapper = modelMapper;
     }
 
@@ -43,5 +47,11 @@ public class SessionController {
     public VoteResponse vote(@PathVariable UUID agendaId, @PathVariable UUID sessionId, @Valid @RequestBody VoteRequest vote) {
         log.info("[POST] - /agendas/{}/session/vote | Request received for user: {}", agendaId.toString(), vote.getDocument());
         return modelMapper.map(voteService.execute(vote.getDocument(),vote.getVote(),sessionId), VoteResponse.class);
+    }
+
+    @GetMapping("{sessionId}")
+    public SessionResultResponse getById(@PathVariable UUID agendaId,@PathVariable UUID sessionId) {
+        log.info("[GET] - /agendas/{}/session/{} | Request received", agendaId.toString(), sessionId);
+        return modelMapper.map(computeVoteService.execute(sessionId), SessionResultResponse.class);
     }
 }
