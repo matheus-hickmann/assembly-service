1ae38a4506fb51651dbe98ae894668bf27ce677b
Matheus Hickmann <hickmann.matheus@gmail.com>
2021-10-07 23:35:01 -0300

    feat: Add service and repository to be able to compute a session

--- src/main/java/com/assembly/assembly/service/repository/VoteRepository.java
+++ src/main/java/com/assembly/assembly/service/repository/VoteRepository.java
@@ -3,10 +3,12 @@ package com.assembly.assembly.service.repository;
 import com.assembly.assembly.service.model.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface VoteRepository extends JpaRepository<Vote, UUID> {
 
     boolean existsBySessionIdAndUserDocument(UUID sessionId, String userDocument);
+    List<Vote> findBySessionId(UUID sessionId);
 
 }
---
+++ src/main/java/com/assembly/assembly/service/service/vote/ComputeVoteService.java
@@ -0,0 +1,44 @@
+package com.assembly.assembly.service.service.vote;
+
+import com.assembly.assembly.service.controller.dtos.SessionResultRecord;
+import com.assembly.assembly.service.enums.VoteEnum;
+import com.assembly.assembly.service.model.Session;
+import com.assembly.assembly.service.model.Vote;
+import com.assembly.assembly.service.repository.VoteRepository;
+import com.assembly.assembly.service.service.session.GetSessionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ComputeVoteService {
+
+    private final GetSessionService getSessionService;
+    private final VoteRepository repository;
+
+    @Autowired
+    public ComputeVoteService(GetSessionService getSessionService,VoteRepository repository) {
+        this.getSessionService = getSessionService;
+        this.repository = repository;
+    }
+
+    public SessionResultRecord execute(UUID sessionId) {
+        Session session = getSessionService.execute(sessionId);
+        List<Vote> votes = repository.findBySessionId(sessionId);
+
+        return new SessionResultRecord(
+                session,
+                countVotes(votes, VoteEnum.YES),
+                countVotes(votes, VoteEnum.NO)
+        );
+    }
+
+    private long countVotes(List<Vote> votes,VoteEnum value) {
+        return votes.stream().filter(vote -> value.equals(vote.getValue())).count();
+
+    }
+
+
+}
